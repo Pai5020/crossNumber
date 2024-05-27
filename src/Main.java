529c602dc1034a90bba25a6193367c5fd083c896
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-05-27 23:53:15 +0530

    revised

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,6 @@ public class Main {
         System.out.println("Enter the rows : ");
         N = sc.nextInt();
 
-       /* for(i=0 ; i<N;  i++){
-            for(j=0;j<2*N;j++){
-                if(i+j==N-1||j-i==N-1 ){
-                    System.out.print(i+1);
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }*/
         for (i = N-1 ; i >= 1; i--) {
             for (j = 0; j < N * 5; j++) {
                 if (i + j == N - 1 || j - i == N - 1) {
